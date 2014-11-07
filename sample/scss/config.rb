09670da17e4e7dfbc624cf51b7d52fdbe1c9d65b
windyakin <windyakin@gmail.com>
2014-11-07 17:40:40 +0900

    デザインラフ実装(#9)の途中まで
    コミットするタイミングを失い中途半端なコミットをしますごめんなさい
    ・scss/にSCSS(Compass)を導入
    ・index.htmlのレイアウト変更

---
+++ sample/scss/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+
+# dev
+http_path		= "/"
+css_dir			= "../css/"
+sass_dir		= "./"
+images_dir		= "../img/"
+javascripts_dir	= "../js/"
+
+output_style	= :expanded
+line_comments	= true	
+
