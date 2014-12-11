3bcb970c3b90a27ea574ce1117dc8883dd962a4f
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-12-11 22:11:52 +0900

    ログイン処理を実装

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@
 require 'bundler'
 Bundler.require
 require './model'
+require './secret'
+
+enable :sessions
+set :session_secret, Secret::KEY
 
 get '/' do
   @title = 'Proconist.net'
@@ -20,4 +24,34 @@ end
 
 get '/css/style.css' do
   sass :stylesheet, :style => :expanded
+end
+
+before '/sign_in' do
+  redirect '/console/' if session[:op_id]
+end
+
+get '/sign_in' do
+  slim :sign_in, :layout => :console_layout
+end
+
+post '/sign_in' do
+  op = Operator.auth(params[:id], params[:password])
+  if op
+    session[:op_id] = op.id
+    redirect '/console/'
+  else
+    redirect "/sign_in?status=error"
+  end
+end
+
+before '/console/*' do
+  unless session[:op_id] && Operator.exists?(id: session[:op_id])
+    session[:op_id] = nil
+    redirect '/sign_in'
+  end
+  @op = Operator.find_by_id(session[:op_id])
+end
+
+get '/console/' do
+  slim :console
 end
