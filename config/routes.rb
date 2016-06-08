516ca57e03057ca661c25e4ab58b5f3812c50d08
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-06-09 08:45:52 +0900

    [WIP]Specific routing table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root    'pages#index'
+
+  get     '/contest',       to: 'contests#index'
+  get     '/contest/:nth',  to: 'contests#show'
+
+  get     '/report',          to: 'reports#index'
+  post    '/report',          to: 'reports#create'
+  get     '/report/thankyou', to: 'reports#thankyou'
+
+  get     '/entry',         to: 'articles#index'
+  get     '/entry/archive', to: 'article_categories#index'
+  get     '/entry/:title',  to: 'articles#show'
+
+  get     '/category/:name',  to: 'article_categories#show'
+
+  get     '/auther/:name',  to: 'operators#show'
+
+  get     '/sign_in',   to: 'sessions#new'
+  post    '/sign_in',   to: 'sessions#create'
+  delete  '/sign_out',  to: 'sessions#destroy'
+
+  get     '/console', to: 'consoles#index'
 end
