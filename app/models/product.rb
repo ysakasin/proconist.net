047d65268bb4d5680eb283773e6d066140e3209e
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-06-10 19:03:58 +0900

    Create method product.section_name

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,17 @@ class Product < ApplicationRecord
   belongs_to :school
 
   has_many :documents
+
+  validates :section, inclusion: { in: [0, 1, 2] }
+
+  SECTIONS = %w(
+    competition
+    themed
+    original
+  )
+
+  def section_name
+    SECTIONS[section]
+  end
+
 end
