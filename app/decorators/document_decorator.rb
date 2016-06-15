3a38e594401edc39b3b6b70c075c2aa28063adc9
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-06-16 01:17:25 +0900

    Create decorator method document.document_type_ja

--- app/decorators/document_decorator.rb
+++ app/decorators/document_decorator.rb
@@ -10,7 +10,22 @@ module DocumentDecorator
     'site':         'fa-globe',
   }.freeze
 
+  DOCUMENT_TYPE_NAME_DICTIONARY = {
+    'github':       'GitHub',
+    'bitbucket':    'Bitbucket',
+    'other_repo':   'リポジトリ',
+    'slideshare':   'SlideShare',
+    'other_slide':  '解説スライド',
+    'twitter':      'Twitter',
+    'facebook':     'Facebook',
+    'site':         'Webサイト',
+  }.freeze
+
   def fa_class
     FA_CLASS_DICTIONARY[document_type]
+  end
+
+  def document_type_ja
+    DOCUMENT_TYPE_NAME_DICTIONARY[document_type.to_sym]
   end
 end
--- spec/decorators/document_decorator_spec.rb
+++ spec/decorators/document_decorator_spec.rb
@@ -25,4 +25,25 @@ describe DocumentDecorator do
       end
     end
   end
+
+  describe '#document_type_ja' do
+    [
+      { 'github':       'GitHub' },
+      { 'bitbucket':    'Bitbucket' },
+      { 'other_repo':   'リポジトリ' },
+      { 'slideshare':   'SlideShare' },
+      { 'other_slide':  '解説スライド' },
+      { 'twitter':      'Twitter' },
+      { 'facebook':     'Facebook' },
+      { 'site':         'Webサイト' }
+    ].each do |document_type, name_ja|
+      context "document_type is #{document_type}" do
+        let(:document) do
+          Fabricate(:document, document_type: document_type).extend(DocumentDecorator)
+        end
+
+        it { expect(document.document_type_ja).to eq name_ja }
+      end
+    end
+  end
 end
