ea5eb50b729fd4ce8cfb2f87b57ae57134c84c86
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-06-19 01:10:21 -0700

    Create contests#show page

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -2,4 +2,9 @@ class ContestsController < ApplicationController
   def index
     @contests = Contest.all.order(nth: :desc)
   end
+
+  def show
+    @contests = Contest.all.order(nth: :desc)
+    @contest = Contest.eager_load(:products).find_by(nth: params[:nth])
+  end
 end
