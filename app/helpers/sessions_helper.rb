a0fb036d76c44d403f4c8dba891a26fa2fc55338
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-07-03 19:53:55 +0900

    Create sessions_helper methods
    - log_in
    - current_operator
    - logged_in?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def log_in(operator)
+    session[:operator_id] = operator.id
+  end
+
+  def current_operator
+    @current_operator ||= Operator.find_by(id: session[:operator_id])
+  end
+
+  def logged_in?
+    !current_operator.nil?
+  end
 end
