67b48191643bca85d7fdda4715df59c682df52e2
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-09-30 00:23:17 +0900

    Creanup meta ogp tags and define helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def page_title
     !@title.blank? ? @title + ' - Proconist.net' : 'Proconist.net'
   end
+
+  def ogp_description
+    @ogp_description || '高専プロコン作品のソースコードやプレゼンテーションまとめ。高専生プロコニストのアップスパイラルを生み出します'
+  end
+
+  def ogp_url
+    @ogp_url || 'https://proconist.net'
+  end
+
+  def ogp_img
+    @ogp_img || 'https://proconist.net/img/ogp.png'
+  end
 end
