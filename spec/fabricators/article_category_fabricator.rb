17b3ef291c1c0b878e92d3ad6063ca3577d97476
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-10-01 18:01:41 +0900

    :squirrel:

--- spec/fabricators/article_category_fabricator.rb
+++ spec/fabricators/article_category_fabricator.rb
@@ -1,8 +1,8 @@
 Fabricator(:article_category) do
   id          1
-  url         "MyString"
-  description "MyString"
-  created_at  "2016-10-01 17:47:43"
-  updated_at  "2016-10-01 17:47:43"
-  name        "MyString"
+  url         'MyString'
+  description 'MyString'
+  created_at  '2016-10-01 17:47:43'
+  updated_at  '2016-10-01 17:47:43'
+  name        'MyString'
 end
