9523b9251414baec055327c93cdba9ca0b3e9680
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-10-09 00:12:39 +0900

    Use form_for in reports#index

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,7 @@
 class ReportsController < ApplicationController
   def index
+    @contests = Contest.all
+    @report = Report.new
+    @sections_enum = { '競技部門': 0, '課題部門': 1, '自由部門': 2 }.freeze
   end
 end
