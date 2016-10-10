529dcd4feb4b397a111c554f3f22f6c4c0a9f8af
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-10-10 18:14:30 +0900

    Add scope for document_type to document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,12 @@
 class Document < ApplicationRecord
   belongs_to :product
+
+  scope :githubs,       -> { where(document_type: 'github') }
+  scope :bitbuckets,    -> { where(document_type: 'bitbucket') }
+  scope :other_repos,   -> { where(document_type: 'other_repo') }
+  scope :slideshares,   -> { where(document_type: 'slideshare') }
+  scope :other_slides,  -> { where(document_type: 'other_slides') }
+  scope :twitters,      -> { where(document_type: 'twitter') }
+  scope :facebooks,     -> { where(document_type: 'facebook') }
+  scope :sites,         -> { where(document_type: 'site') }
 end
