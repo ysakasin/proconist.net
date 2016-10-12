be1513ff133274b515018c3a603f33bc3cf87bd0
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-10-13 00:08:29 +0900

    Create 'type' method to history model

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,12 @@
 class History < ApplicationRecord
+  def type
+    case label
+    when 0
+      :product
+    when 1
+      :entry
+    when 2
+      :notice
+    end
+  end
 end
--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe History, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#type' do
+    subject do
+      history.type
+    end
+
+    [
+      { label: 0, type: :product },
+      { label: 1, type: :entry },
+      { label: 2, type: :notice }
+    ].each do |set|
+      context "label is #{set[:label]}" do
+        let(:history) { Fabricate(:history, label: set[:label]) }
+        it { should eq set[:type] }
+      end
+    end
+  end
 end
