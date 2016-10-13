92bc7941d14c6684f9072a1126d08885bdb75d5b
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-10-13 23:26:46 +0900

    Render sidebar as partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 
   def index
     @contest = Contest.last
+    @contests = Contest.all.order(nth: :desc)
+    @histories = History.all.order(created_at: :desc)
+    @histories_with_image = History.where.not(image_path: nil).where(label: 1).limit(5)
   end
 
   def about
