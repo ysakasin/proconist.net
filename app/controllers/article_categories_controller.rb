e41151f87bf6cba0720515eaaf8a65fcd5d2f2e7
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-11-10 23:18:19 +0900

    Porting archive view

--- app/controllers/article_categories_controller.rb
+++ app/controllers/article_categories_controller.rb
@@ -4,4 +4,9 @@ class ArticleCategoriesController < ApplicationController
     @category = ArticleCategory.includes(:articles).find_by(url: params[:url])
     @categories = ArticleCategory.all
   end
+
+  def index
+    @articles = Article.published.all.order(created_at: :desc)
+    @categories = ArticleCategory.includes(:articles).all
+  end
 end
