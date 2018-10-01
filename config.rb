b657a90bbcd0a48786d66ab40da56d1d66339ce3
SAKATA Sinji <sinji@sakasin.net>
2018-10-02 08:26:31 +0900

    Port each contest page

--- config.rb
+++ config.rb
@@ -23,13 +23,9 @@ page '/*.txt', layout: false
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+data.contests.each do |id, contest|
+  proxy "/contest/#{id.to_i}/index.html", "/products.html", locals: {contest: contest}, ignore: true
+end
 
 # Helpers
 # Methods defined in the helpers block are available in templates
